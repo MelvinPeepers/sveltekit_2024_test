bda57ccee7035eda673fd5d62f2fe4e3356609c1
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-04-10 13:29:17 -0500

    redoing toml

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,6 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       edge: true,
-      split: false,
     }),
   },
 };
