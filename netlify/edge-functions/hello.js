dc5fc53af983b0af55dbb62a67d801bb58860f36
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-04-10 12:20:33 -0500

    changed edge function to netlify example

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,8 +1,3 @@
-export default async (request, context) => {
-  return new Response("Hello World!", {
-    status: 200,
-    headers: {
-      "content-type": "text/plain",
-    },
-  });
-};
+export default () => new Response("Hello world");
+
+export const config = { path: "/test" };
