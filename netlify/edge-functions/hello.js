cb86b4238c6c69376fdb7ebc6b9d6dee72203f48
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-04-10 13:05:26 -0500

    redo

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,8 @@
-export default () => new Response("Hello world");
-
-export const config = { path: "/test" };
+export default async (request, context) => {
+  return new Response("Hello World from the Edge!", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
+};
